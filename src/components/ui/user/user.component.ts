2fdebb37cf9edf242bf804795197f3aba308a774
brechtbilliet <billietbrecht@gmail.com>
2023-04-20 09:03:37 +0200

    5 files modified

--- src/components/ui/user/user.component.ts
+++ src/components/ui/user/user.component.ts
@@ -1,19 +1,21 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LoadingDirective } from '../../../loading.directive';
 import { User } from '../../../types/user';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  imports: [NgOptimizedImage, CommonModule],
+  imports: [NgOptimizedImage, CommonModule, LoadingDirective],
+  hostDirectives: [
+    {
+      directive: LoadingDirective,
+      inputs: ['loading'],
+    },
+  ],
   standalone: true,
   styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {
   @Input() public user: User | null = null;
-  @Input() loading = false;
-  @HostBinding('class.loading') get loadingClass() :boolean {
-    return this.loading;
-  }
 }
